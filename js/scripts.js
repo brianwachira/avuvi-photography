ff5733dd93e3ab6c0a95ecd03ab5f62e5725e8bf
brianwachira <brianwachira7@gmail.com>
2019-01-03 12:20:21 +0300

    add jquery to expriment on navbar active

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // $("a").click(function(event) {
+    //     $(".active").removeClass("active");
+    //     $(this).addClass("active");
+    //     event.preventDefault();
+    // });
+});
